51f045e9642e1f957f85c03099790f4c9db3f1fc
jaahnavi <jahnavireddybodepati@gmail.com>
2025-08-24 21:25:59 +0530

    sum of digits

---
+++ sum_of_digits.py
@@ -0,0 +1,10 @@
+
+t = int(input())
+for i in range(t):
+    x = int(input())
+    sum_digits = 0
+    while x != 0:
+        digit = x % 10       # Extract the last digit
+        sum_digits += digit  # Add the digit to the sum
+        x //= 10             # Integer division to remove the last digit
+    print(sum_digits)        # Print the sum for the current test case
